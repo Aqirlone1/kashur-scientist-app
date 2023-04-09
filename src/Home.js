c596652bf8ec73b3a89757aa972b993e577506d1
Aqirlone1 <aqirlone109@gmail.com>
2023-04-09 22:23:41 +0530

    added courses card

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import HomePage from './HomePage'
 import CardList from './CardList'
+import CoursesCard from './CoursesCard/CoursesCard'
 
 const Home = () => {
   return (
     <div>
         <HomePage />
+        <CoursesCard />
         <CardList />
-       
-
         
-
     </div>
   )
 }
