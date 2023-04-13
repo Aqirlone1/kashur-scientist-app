e40e43dc600ff225de3c0fb46f033aeb419b5ace
Aqirlone1 <aqirlone109@gmail.com>
2023-04-13 18:09:26 +0530

    added feedback cards

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import HomePage from './HomePage'
 import CardList from './CardList'
 import CoursesCard from './CoursesCard/CoursesCard'
+import FeedBackCards from './feedback/FeedBackCards'
 
 const Home = () => {
   return (
     <div>
         <HomePage />
         <CoursesCard />
+        <FeedBackCards />
         <CardList />
         
     </div>
---
+++ src/feedback/FeedBackCards.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './FeedbackCards.css'
+
+const FeedBackCards = props => {
+
+return(
+  <>
+  <div className="main-div">
+  <h1 className="heading-text">WHAT STUDENTS SAY ABOUT US</h1>
+   <div className="flex-container-two">
+    <div className="card-two">
+     <p> Well, there are several reasons. 
+      Firstly, Python is a powerful language that is used by many top companies such as Google,
+       Facebook, Amazon, and Instagram. It has become the language of choice for many 
+       </p>
+    </div>
+    <div className="card-two">
+     <p> Well, there are several reasons. 
+      Firstly, Python is a powerful language that is used by many top companies such as Google,
+       Facebook, Amazon, and Instagram. It has become the language of choice for many 
+       </p>
+    </div>
+    
+    <div className="card-two">
+     <p> Well, there are several reasons. 
+      Firstly, Python is a powerful language that is used by many top companies such as Google,
+       Facebook, Amazon, and Instagram. It has become the language of choice for many 
+       </p>
+    </div>
+    </div>
+   </div>
+  </>
+)
+}
+export default FeedBackCards
+
