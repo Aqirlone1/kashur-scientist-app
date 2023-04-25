b17bf0ebb1b2f138113e01919c2e7e5bbe73b1a3
Mir-Tasleem <mirtasleem1234@gmail.com>
2023-04-25 18:05:00 +0530

    internalpages added

--- src/Goals.js
+++ src/Goals.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+
 import './Goals.css';
 
 
@@ -11,7 +13,9 @@ function Goals() {
       <div className="part2G">
       <h1 id="cardheadG">Exam Preperation</h1>
       <p>KOSHUR SCIENTIST, a leading educational institute in Kashmir, has taken a taken important step towards educating people about different competative exams like NEET,JEE,NTSE,OLYMPIADS etc.</p>
-      <Link to="/exam" id="btn">Know More</Link>
+      <Link to="/Exam">
+       <button id="btn">Know More</button> 
+      </Link>
       </div>
 
       </div>
@@ -21,7 +25,9 @@ function Goals() {
       <div className="part1-MH">
       <h1 id="cardheadG">Mental Health</h1><br/>
       <p>KOSHUR SCIENTIST, a leading educational institute in Kashmir, has taken a significant step towards promoting mental health awareness in schools. </p>
-      <Link to="mentalhealth" id ="btn">Know More</Link>
+      <Link to="/Mentalhealth">
+      <button id="btn">Know More</button> 
+      </Link>{" "}
 
       </div>
       <div className="part2-emt"></div>
@@ -35,7 +41,9 @@ function Goals() {
       <div className="part2G">
       <h1 id="cardheadG">Skill Orientation</h1><br/>
       <p>KOSHUR SCIENTIST, a leading educational institute in Kashmir, has taken an important step towards promoting skill awareness among students. The institute has launched a program that involves visiting different schools and organizing various events to make students aware of market needs and demanding skills. </p>
-      <Link to="skill" id="btn">Know More</Link>
+      <Link to="/Skill">
+      <button id="btn">Know More</button> 
+      </Link>
       </div>
       </div>
 
---
+++ src/Skill.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Skill() {
+  return (
+    <div>
+        <h1>Skill Orientation</h1>
+        <p>KOSHUR SCIENTIST, a leading educational institute in Kashmir, has taken an important step towards promoting skill awareness among students. The institute has launched a program that involves visiting different schools and organizing various events to make students aware of market needs and demanding skills. The program aims to help students become market-ready at a very early age so that they can get employed in their desired industries.
+        The events organized under this program aim to educate students about the latest market trends and the skills that are in demand in various industries. The program also includes workshops and seminars conducted by industry professionals who provide guidance and support to students. The events are designed to help students develop the necessary skills to excel in their chosen fields.
+        The skill awareness program is an excellent initiative by KOSHUR SCIENTIST, as it addresses an issue that is often overlooked in the education system. Many students graduate from schools without the necessary skills to excel in their chosen fields, and this program provides them with the necessary support and guidance they need to become industry-ready.
+        The program is an excellent opportunity for students to learn about the latest market trends and the skills that are in demand in various industries. It also promotes a culture of excellence and encourages students to develop the necessary skills to excel in their chosen fields. The program aims to produce skilled industry-ready students who are equipped to tackle the challenges of the modern world.
+        In conclusion, KOSHUR SCIENTIST's skill awareness program is an excellent initiative that provides much-needed support and guidance to students. The program's focus on visiting different schools and organizing various events is an effective way to spread awareness and educate students about the latest market trends and the skills that are in demand in various industries. It is an excellent opportunity for students to learn about the latest market trends and develop the necessary skills to excel in their chosen fields. The program promotes a culture of excellence and 
+        aims to produce skilled industry-ready students who are equipped to tackle the challenges of the modern world.</p>
+    </div>
+  )
+}
+
+export default Skill
