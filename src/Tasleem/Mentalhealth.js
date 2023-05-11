1e58caa36f1eb6cac49ee6063ab0fb2bb1893e8e
Mir-Tasleem <mirtasleem1234@gmail.com>
2023-05-11 22:44:19 +0530

    fonts and images added

--- src/Tasleem/Mentalhealth.js
+++ src/Tasleem/Mentalhealth.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Goals.css'
+import img1 from './images/img1.jpeg'
+import android from './images/android.jpeg'
+import python from './images/python.jpeg'
+import webd from './images/webd.jpeg'
 
 function Mentalhealth() {
   return (
     <div className="MHealth">
+    
         <h1 id="MHead">MENTAL HEALTH</h1>
         <p className="MHpara1">KOSHUR SCIENTIST, a leading educational institute in Kashmir, has taken a significant step towards promoting mental health awareness in schools. The institute has launched a program that involves visiting different schools and organizing various events to make people aware of mental health and related issues. The program aims to help students deal with the difficulties they face in academics, which can have a significant impact on their mental health
         The events organized under this program aim to educate students about various aspects of mental health, including stress management, anxiety, depression, and other mental health issues. The program also includes workshops and seminars conducted by mental health professionals who provide guidance and support to students. The events are designed to help students develop coping mechanisms and strategies to deal with the challenges they face in their daily lives.
         The mental health awareness program is an excellent initiative by KOSHUR SCIENTIST, as it addresses an issue that is often overlooked in the education system. Many students struggle with mental health issues due to the pressure of academic performance, and this program provides them with the necessary support and guidance they need to deal with these issues.
         The program is an excellent opportunity for students to learn about mental health and related issues and develop the necessary skills to deal with stress and anxiety. It also promotes a culture of openness and encourages students to speak up about their mental health issues and seek help when needed.
         In conclusion, KOSHUR SCIENTIST's mental health awareness program is an excellent initiative that provides much-needed support and guidance to students struggling with mental health issues. The program's focus on visiting different schools and organizing various events is an effective way to spread awareness and educate students about mental health. It is an excellent opportunity for students to learn about mental health and related issues and develop the necessary skills to deal with stress and anxiety. The program promotes a culture of openness and encourages
-         students to seek help when needed, which is critical for their mental well-being.</p>
+         students to seek help when needed, which is critical for their mental well-being.</p>        
     </div>
+
   )
 }
 
